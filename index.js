085112879ebf60c358b6164fe3c62621cb25f053
glyfy <glenlimyf@gmail.com>
2022-06-22 23:03:56 +0800

    Revert "Revert "Merge branch 'chat-app' into development""
    This reverts commit 5f7efb34d0003f739dc09a3b22e186659f90dffc.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const path = require("path");
 const multer = require("multer");
 const { json, cookie } = require("express/lib/response");
@@ -19,7 +21,8 @@ const session = require('express-session');
 
 
 dotenv.config();
-
+//mongodb+srv://glyfy:glyfy@cluster0.j2bnm.mongodb.net/orbital?retryWrites=true&w=majority
+//mongodb+srv://sanjevi13:T0nYSt4rk@cluster0.itmtd.mongodb.net/?retryWrites=true&w=majority
 mongoose.connect('mongodb+srv://glyfy:glyfy@cluster0.j2bnm.mongodb.net/orbital?retryWrites=true&w=majority', {useNewURLParser: true}, () => {
     console.log("Connected to MongoDB");
 });
@@ -55,6 +58,8 @@ app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 //uploading files to server
 const storage = multer.diskStorage({
---
+++ socket/index.js
@@ -0,0 +1,51 @@
+const io = require("socket.io")(8900, {
+    cors:{
+        origin: "http://localhost:3000",
+    },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) && //ensure user does not already exist
+        users.push({userId, socketId});
+};
+
+// delete user by filtering users array
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    //look through array and find first user that has given userId
+    return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+    // connect user
+    console.log("user connected")
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        //console.log(users);
+        io.emit("getUsers", users) 
+    });
+
+    //send and get message
+    socket.on("sendMessage", ({senderId, receiverId, text}) => {
+        console.log(`users array: ${JSON.stringify(users)}`)
+        const user = getUser(receiverId);
+        if (user){
+            io.to(user.socketId).emit("getMessage", {
+                senderId,
+                text,
+            });
+        }
+    });
+
+    // disconnect user
+    socket.on("disconnect", () =>{
+        console.log("user disconnected")
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+});
