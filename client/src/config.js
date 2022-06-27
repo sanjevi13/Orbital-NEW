927d6ec61062f6a627976fdd17d46495321eecea
sanjevi13 <e0726583@u.nus.edu>
2022-06-27 20:49:48 +0800

    my first commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:5000/api"
+    baseURL : "https://nusconnectm2.herokuapp.com/api/"
     //set header to include "Access-Control-Allow-Origin" : "*"
     //https://stackoverflow.com/questions/45975135/access-control-origin-header-error-using-axios
 });
