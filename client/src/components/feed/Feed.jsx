24c540c514e26f363050739f7323fa73c7f66b21
sanjevi13 <e0726583@u.nus.edu>
2022-06-29 20:51:34 +0800

    try fix bug

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -15,7 +15,7 @@ export default function Feed({username}) {
       const res = username //if we are looking at another user
         ? await axios.get("/posts/profile/" + username) 
         : await axios.get("/posts/timeline/" + user._id);
-        
+        console.log(res.data);
         setPosts(res.data.sort((p1,p2) => {
           return new Date(p2.createdAt) - new Date(p1.createdAt);
         }));
