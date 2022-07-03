d7ccae1055f7ccae6e4e4dc731f95320ee8acc61
glyfy <glenlimyf@gmail.com>
2022-07-03 22:18:09 +0800

    a

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -12,9 +12,10 @@ export default function Rightbar({user}) { //user refers to user that rightbar i
   const [friends, setFriends] = useState([]);
   const {user: currentUser, dispatch} = useContext(AuthContext);
   {console.log(user?._id)}
-  {console.log(currentUser.following)}
-  {console.log(currentUser.following.includes(user?._id))}
+  {console.log(currentUser.following)};
+  {console.log(currentUser.following.includes(user?._id))};
   const [followed,  setFollowed] = useState(currentUser.following.includes(user?._id));
+  {console.log(followed)};
 
   
   useEffect(()=> { //obtain all of user's friends 
